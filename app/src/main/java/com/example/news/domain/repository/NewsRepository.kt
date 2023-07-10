6c2c3e8056066a7c620e431b7dfc3fd6ff7b6b65
Horacio <oicaroh64@gmail.com>
2023-07-10 12:10:31 -0300

    Creation of models

--- app/src/main/java/com/example/news/domain/repository/NewsRepository.kt
+++ app/src/main/java/com/example/news/domain/repository/NewsRepository.kt
@@ -1,5 +1,16 @@
 package com.example.news.domain.repository
 
+import com.example.news.data.model.APIResponse
+import com.example.news.data.model.Article
+import com.example.news.data.util.Resource
+
 interface NewsRepository {
+
+    suspend fun getNews(): Resource<APIResponse>
+    suspend fun getSearchedNews(): Resource<APIResponse>
+    suspend fun saveNews(article: Article)
+    suspend fun deleteNews(article: Article)
+    fun getSavedNews(): List<Article>
+
 
 }

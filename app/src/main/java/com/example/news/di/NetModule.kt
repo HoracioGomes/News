ef6e74895742dfaa4078a628567823bceedb09da
Horacio <oicaroh64@gmail.com>
2023-07-13 12:01:31 -0300

    Hilt initial implementation and net module creation

---
+++ app/src/main/java/com/example/news/di/NetModule.kt
@@ -0,0 +1,22 @@
+package com.example.news.di
+
+import com.example.news.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetModule {
+
+    @Provides
+    fun providesRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BuildConfig.BASE_URL)
+            .build()
+    }
+}

6689f6f262852d73d2dd4409b5254d4450a027be
Horacio <oicaroh64@gmail.com>
2023-07-13 12:13:10 -0300

    Change Retrofit to singleton

--- app/src/main/java/com/example/news/di/NetModule.kt
+++ app/src/main/java/com/example/news/di/NetModule.kt
@@ -7,12 +7,14 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class NetModule {
 
     @Provides
+    @Singleton
     fun providesRetrofit(): Retrofit {
         return Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
